47e43544449e7780018bbcfee736b67f3655d135
Aritra Nandi <Aritran@gain-insights.com>
2023-08-22 17:40:22 +0530

    title correction

--- module4_exrecise/src/Components/Concepts.jsx
+++ module4_exrecise/src/Components/Concepts.jsx
@@ -5,7 +5,7 @@ const Concepts = (props) => {
     <>
       <li className="concept">
         <img src={props.img} alt={props.title} />
-        <h2>{props.tite}</h2>
+        <h2>{props.title}</h2>
         <p>{props.desc}</p>
       </li>
     </>
