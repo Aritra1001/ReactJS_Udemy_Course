8d0b44bcd0e0f2b7ba62b05a50f2adf3b16860ae
Aritra Nandi <Aritran@gain-insights.com>
2023-08-31 12:13:08 +0530

    bump animation added in header cart button

--- food_order_app/src/Components/Layout/HeaderCartButton.jsx
+++ food_order_app/src/Components/Layout/HeaderCartButton.jsx
@@ -1,20 +1,42 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
 
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  
   // By using useContext in this component, this component will be re-evaluated whenever the context is updated.
-
   let ctxData = useContext(CartContext);
-  let numberOfCartItems = ctxData.items.reduce((currNumber, item)=>{
+
+  const {items} = ctxData;
+  let numberOfCartItems = items.reduce((currNumber, item)=>{
     return currNumber + item.amount;
   }, 0)
 
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(()=>{
+    
+    if(items.length === 0){
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    let timer = setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return ()=>{
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
     <>
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon/>
             </span>
