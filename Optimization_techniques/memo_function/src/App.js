4f0c7d74ba9af5d7117c745bdda17f3d33e35e5b
Aritra Nandi <Aritran@gain-insights.com>
2023-09-01 14:35:38 +0530

    useCallback and its dependencies and useMemo hook

--- Optimization_techniques/memo_function/src/App.js
+++ Optimization_techniques/memo_function/src/App.js
@@ -7,17 +7,27 @@ import Button from './Components/UI/Button/Button';
 function App() {
 
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
+  const allowToggleHandler = () =>{
+     setAllowToggle(true);
+  }
+
   const toggleParagraphHandler = useCallback(() =>{
-    setShowParagraph((prevState)=> !prevState);
-  }, []);
+    if(allowToggle){
+      setShowParagraph((prevState)=> !prevState);
+    }
+  }, [allowToggle]); 
+  //We are passing the dependency of the callback so that whenever 
+  // that state changes useCallback stores the function in the memory with the updated values
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
