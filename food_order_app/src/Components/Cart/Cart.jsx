1f384b85fde2efa35790bcdab18f73c526aba91b
Aritra Nandi <Aritran@gain-insights.com>
2023-09-14 14:37:25 +0530

    fetching meals data from firebase and added a checkout form

--- food_order_app/src/Components/Cart/Cart.jsx
+++ food_order_app/src/Components/Cart/Cart.jsx
@@ -1,41 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const ctxData = useContext(CartContext);
 
   const totalAmount = ctxData.totalAmount.toFixed(2);
   const hasItems = ctxData.items.length > 0;
 
-  const cartItemAddHandler = item => {
-    ctxData.addItem({...item, amount: 1})
-  }
+  const cartItemAddHandler = (item) => {
+    ctxData.addItem({ ...item, amount: 1 });
+  };
 
-  const cartItemRemoveHandler = id => {
+  const cartItemRemoveHandler = (id) => {
     ctxData.removeItem(id);
-  }
+  };
+
+  const checkoutHandler = () => {
+    setIsCheckout(true);
+  };
 
   const cartItem = (
     <ul className={styles["cart-items"]}>
       {ctxData.items.map((item) => {
         return (
-            <CartItem
-              key= {item.id}
-              name= {item.name}
-              price= {item.price}
-              amount= {item.amount}
-              onRemove= {cartItemRemoveHandler.bind(null, item.id)} 
-              /* bind pre configures the function for future execution and also pre-configures 
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            amount={item.amount}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            /* bind pre configures the function for future execution and also pre-configures 
               the argument that function will receive when it is executed  */
-              onAdd= {cartItemAddHandler.bind(null, item)}
-            />
-            // <li>{item.name}</li>
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+          // <li>{item.name}</li>
         );
       })}
     </ul>
+  );
+
+  const cartActions = (
+    <div className={styles.actions}>
+      <button className={styles["button--alt"]} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={styles.button} onClick={checkoutHandler}>
+          Order
+        </button>
+      )}
+    </div>
   );
 
   return (
@@ -46,13 +65,8 @@ const Cart = (props) => {
           <span>Total Amount</span>
           <span>{`$${totalAmount}`}</span>
         </div>
-
-        <div className={styles.actions}>
-          <button className={styles["button--alt"]} onClick={props.onHideCart}>
-            Close
-          </button>
-          {hasItems && <button className={styles.button}>Order</button>}
-        </div>
+        {isCheckout && <Checkout onClose={props.onHideCart}/>}
+        {!isCheckout && cartActions}
       </Modal>
     </>
   );
