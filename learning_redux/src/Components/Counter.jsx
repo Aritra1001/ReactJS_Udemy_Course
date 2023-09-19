4072e561116069c64f75d736e4bc5634ee219ae4
Aritra Nandi <Aritran@gain-insights.com>
2023-09-19 17:57:00 +0530

    redux in class based compo, adding payload to dispatch function, handling multiple states using redux

--- learning_redux/src/Components/Counter.jsx
+++ learning_redux/src/Components/Counter.jsx
@@ -1,29 +1,37 @@
-import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux';
+import classes from "./Counter.module.css";
+// import { Component } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-
-  const counter = useSelector((state)=> state.counter); //useSelector hook etablishes the subscription to the redux store for thic component
+  const counter = useSelector((state) => state.counter); //useSelector hook etablishes the subscription to the redux store for thic component
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-    
-  const toggleCounterHandler = () => {};
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
-  }
+    dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
-  }
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>  
+      {show && <div className={classes.value}>{counter}</div>}
       {/* Here we need to output the current counter. For that we need to get access
       to the redux store and the data in there. For that we are using useSelector hook from react-redux library. */}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -32,3 +40,46 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// class Counter extends Component {
+//   incrementHandler = () => {
+//     this.props.increment();
+//   };
+
+//   decrementHandler = () => {
+//     this.props.decrement();
+//   };
+
+//   toggleCounterHandler = () => {};
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         {/* Here we need to output the current counter. For that we need to get access
+//         to the redux store and the data in there. For that we are using useSelector hook from react-redux library. */}
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
